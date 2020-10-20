c72468074d83ede00959872d4a9806c02d8c040d
roofroot <565749553@qq.com>
2020-10-20 17:37:51 +0800

    Update BaseActivity.kt

--- app/src/main/java/com/my/test2/kotlin/BaseActivity.kt
+++ app/src/main/java/com/my/test2/kotlin/BaseActivity.kt
@@ -99,36 +99,5 @@ abstract class BaseActivity<T : ViewBinding, M : BaseModel<*>> :
     protected abstract fun onPrepare()
     protected abstract fun getBindingInstance(): T
     protected abstract fun getModelInstance(): M
-    fun setStateBar(textDark: Boolean) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                //5.x开始需要把颜色设置透明，否则导航栏会呈现系统默认的浅灰色
-                val window = window
-                val decorView = window.decorView
-                //两个 flag 要结合使用，表示让应用的主体内容占用系统状态栏的空间
-                if (textDark) {
-                    val option =
-                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-                    decorView.systemUiVisibility = option
-                } else {
-                    val option =
-                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    decorView.systemUiVisibility = option
-                }
-                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-                window.statusBarColor = Color.TRANSPARENT
-                //导航栏颜色也可以正常设置
-//                window.setNavigationBarColor(Color.TRANSPARENT);
-            } else {
-                val window = window
-                val attributes = window.attributes
-                val flagTranslucentStatus = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-                val flagTranslucentNavigation =
-                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
-                attributes.flags = attributes.flags or flagTranslucentStatus
-                //                attributes.flags |= flagTranslucentNavigation;
-                window.attributes = attributes
-            }
-        }
-    }
+ 
 }
